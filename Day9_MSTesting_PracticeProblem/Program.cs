fd4446a7887f1629f80e0fc3fb1f053ea5613354
mahesh <gangurdemahesh9@gmail.com>
2023-04-02 16:35:23 +0530

    [mahesh]add Welcome Greeting Message

---
+++ Day9_MSTesting_PracticeProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9_MSTesting_PracticeProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Day 9 MS tesing Practice Problem");
+
+        }
+    }
+}
